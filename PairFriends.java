698579ed1d2f588957f44d8cec698cd1e80ea293
prince-kumar-yadav06 <163705172+prince-kumar-yadav06@users.noreply.github.com>
2025-04-27 20:50:09 +0530

    Create PairFriends.java

---
+++ PairFriends.java
@@ -0,0 +1,23 @@
+public class PairFriends {
+    
+    public static int FindPair(int n){
+
+        if( n == 1 || n==2){
+            return 1;
+        }
+
+        //  for single 
+        int fn1=FindPair(n-1);
+
+        // for double 
+        int fn2=FindPair(n-2);
+
+        return fn1+ fn1*fn2;
+    }
+
+    public static void main(String args[]){
+        int n=2;
+
+        System.out.println(FindPair(n));
+    }
+}
